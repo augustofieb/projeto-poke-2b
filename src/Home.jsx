683b4699280cc647217d42ccba1a8c30bf06c23d
augustofieb <rm94051@estudante.fieb.edu.br>
2025-05-19 12:08:41 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
 import poke from "./assets/pokeapi_256.png"
+import eevee from './assets/image.png'
+import './Home.css'
 
 const Home = () => {
     return (
         <div className="pokeflex"> 
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
-            <img src={poke} />
+            <img src={eevee} />
         </div>
     )
 }
